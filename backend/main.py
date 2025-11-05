1f4a2dc21b68d858310e5c2bcdc8a95e96e2c089
cnniranjan72 <cnniranjan72@gmail.com>
2025-11-05 07:55:36 +0530

    Final fixes

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from app import create_app
+
+# Create the Flask app instance for Gunicorn
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
